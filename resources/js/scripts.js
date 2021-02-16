801a66947f65b135e52b56f20bb8187ffdecaa13
Marina Potapova <marinapotapova08@gmail.com>
2021-02-15 22:52:42 -0600

    added amimation to navigation

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -45,4 +45,92 @@ $(document).ready(function() {
         }
 });
 });
+    
+    /*animation on scroll*/
+     $('.js--wp-1').waypoint(
+    (direction) => {
+      $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    },
+    {
+      offset: '50%',
+    }
+  );
+      $('.js--wp-2').waypoint(
+    (direction) => {
+      $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    },
+    {
+      offset: '50%',
+    }
+  );
+    
+    $('.js--header').waypoint(
+    (direction) => {
+      if (direction === 'up') {
+        $('.js--wp-1').removeClass('animate__animated animate__fadeIn');
+        $('.js--wp-2').removeClass('animate__animated animate__fadeInUp');
+      }
+    },
+    {
+      offset: '-25%'
+    }
+  );
+    
+    
+    /*Mobile navigation*/
+    
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon ion-icon');
+        nav.slideToggle(200);
+        
+        /*if(icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        }
+        else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }*/
+     if (icon.attr("name") === 'menu') {
+          icon.attr("name", 'close' );
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+         
+        } else {
+          icon.attr("name", 'menu');
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+    
+   
+    $(window).resize(function(){
+
+var nav = $('.js--main-nav');
+
+var icon = $('.js--nav-icon i');
+
+if ($(window).width() > 767){
+
+nav.css("display", "block");
+
+icon.addClass('ion-close-round');
+
+icon.removeClass('ion-navicon-round');
+
+} else {
+
+nav.css("display", "none");
+
+icon.addClass('ion-navicon-round');
+
+icon.removeClass('ion-close-round');
+
+}
+
+});
+ 
+
+
 });

bec846d6657987c1f5e435f911b544b9e7d85bae
Marina Potapova <marinapotapova08@gmail.com>
2021-03-11 13:07:09 -0600

    added render() method to insert data ti the table and populated 4 examples of Home price dinamically

--- resources/js/table.js
+++ resources/js/table.js
@@ -1,5 +1,5 @@
 "use strict";
-var TBL = [];
+var TBL = []; //array of objects to store the table data
 
 // $("table#details tr").each(function () {
 //   var arrayOfThisRow = [];
@@ -37,8 +37,8 @@ $("#details thead td").each(function () {
 //   }
 // });
 // console.log(xValue);
-
-for (var i = 0; i < 5; i++) {
+// add objects to the TBl variable
+for (var i = 0; i < 0; i++) {
   var obj = {};
   for (var j = 0; j < xKey.length; j++) {
     obj[xKey[j]] = "";
@@ -46,4 +46,105 @@ for (var i = 0; i < 5; i++) {
   TBL.push(obj);
 }
 
-//console.log(TBL);
+// insert data to TBL
+function render() {
+  var table = document.getElementById("datas");
+  table.innerHTML = "";
+  var tr = "";
+  TBL.forEach((x) => {
+    tr += "<tr>";
+    tr +=
+      "<td>" +
+      x["Home Price"] +
+      "</td>" +
+      "<td>" +
+      x["Credit Score"] +
+      "</td>" +
+      "<td>" +
+      x["Interest Rate"] +
+      "</td>" +
+      "<td>" +
+      x["Monthly Rent"] +
+      "</td>" +
+      "<td>" +
+      x["Monthly Savings"] +
+      "</td>" +
+      "<td>" +
+      x["Initial Budget"] +
+      "</td>" +
+      "<td>" +
+      x["DP, %"] +
+      "</td>" +
+      "<td>" +
+      x["DP, $"] +
+      "</td>" +
+      "<td>" +
+      x["Mon Sav"] +
+      "</td>" +
+      "<td>" +
+      x["Loan"] +
+      "</td>" +
+      "<td>" +
+      x["LTV"] +
+      "</td>" +
+      "<td>" +
+      x["Type"] +
+      "</td>" +
+      "<td>" +
+      x["PMT"] +
+      "</td>" +
+      "<td>" +
+      x["Extra"] +
+      "</td>" +
+      "<td>" +
+      x["PMI Rate"] +
+      "</td>" +
+      "<td>" +
+      x["PMI Monthly"] +
+      "</td>" +
+      "<td>" +
+      x["PMI Payment"] +
+      "</td>" +
+      "<td>" +
+      x["Property Taxes"] +
+      "</td>" +
+      "<td>" +
+      x["Home Insurence"] +
+      "</td>" +
+      "<td>" +
+      x["Monthly"] +
+      "</td>" +
+      "<td>" +
+      x["Mortgage"] +
+      "</td>" +
+      "<td>" +
+      x["Rent"] +
+      "</td>" +
+      "<td>" +
+      x["S+M+R"] +
+      "</td>" +
+      "<td>" +
+      x["Overpay"] +
+      "</td>";
+    tr += "</tr>";
+  });
+  table.innerHTML += tr;
+}
+
+// dinamically populate 4 elements into TBL
+var price = 350000;
+for (let i = 0; i < 4; i++) {
+  var newPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumSignificantDigits: Math.trunc(Math.abs(price)).toFixed().length,
+  }).format(price);
+  price += 50000;
+  TBL.push({
+    "Home Price": newPrice,
+  });
+}
+
+render();
+
+console.log(TBL);
